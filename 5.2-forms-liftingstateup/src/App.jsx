24beeba5e3e1d0a74d69f02d192b6be91842e238
Zidane Ibrahim Fadela <zidaneibrhmfdl7@gmail.com>
2023-08-16 20:49:39 +0700

    update data

--- 5.2-forms-liftingstateup/src/App.jsx
+++ 5.2-forms-liftingstateup/src/App.jsx
@@ -1,22 +1,6 @@
-import React, { useEffect } from 'react';
+
 
 function App() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const authorizationCode = urlParams.get('code');
-
-  useEffect(() => {
-    async function exchangeAuthorizationCodeForToken() {
-      if (authorizationCode) {
-        const response = await fetch('http://localhost:3000/callback?code=' + authorizationCode);
-        const data = await response.json();
-        const accessToken = data.accessToken;
-        console.log('Access Token:', accessToken);
-      }
-    }
-
-    exchangeAuthorizationCodeForToken();
-  }, [authorizationCode]);
-
   return (
     <>
       <div className="container">
